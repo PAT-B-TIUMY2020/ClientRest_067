6e0cfe69fe090ed273b87f403c2509da089bcd3f
YusufJake <yusufjohan05@gmail.com>
2020-12-21 21:02:24 +0700

    Client Rest sudah siap

---
+++ ClassData.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Runtime.Serialization;
+using System.Net;
+using static ClientRest_20180140067_Yusuf_Johan_Kelana.Program;
+
+namespace ClientRest_20180140067_Yusuf_Johan_Kelana
+{
+    class ClassData
+    {
+        public void getData()
+        {
+            // var type data yang tidak peduli pada bentuk, tapi isinya
+            var json = new WebClient().DownloadString("http://localhost:1907/Mahasiswa");
+            var data = JsonConvert.DeserializeObject<List<Mahasiswa>>(json);
+            /* for (int i = 0; i < data.Count; i++)
+             * {
+             *      Console.WriteLine(data[i].nama);
+             *      Console.WriteLine(data[i].nim);
+             *      Console.WriteLine(data[i].prodi);
+             *      Console.WriteLine(data[i].angkatan);
+             * }
+             */
+
+            foreach (var mahasiswa in data)
+            {
+                Console.WriteLine("NIM: " + mahasiswa.nama);
+                Console.WriteLine("Nama: " + mahasiswa.nim);
+                Console.WriteLine("Prodi: " + mahasiswa.prodi);
+                Console.WriteLine("Angkatan: " + mahasiswa.angkatan);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Runtime.Serialization;
+using System.Net;
+
+namespace ClientRest_20180140067_Yusuf_Johan_Kelana
+{
+    class Program
+    {
+        [DataContract]
+        public class Mahasiswa
+        {
+            private string _nama, _nim, _prodi, _angkatan;
+
+            [DataMember]
+            public string nama
+            {
+                get { return _nama; }
+                set { _nama = value; }
+            }
+
+            [DataMember]
+            public string nim
+            {
+                get { return _nim; }
+                set { _nim = value; }
+            }
+
+            [DataMember]
+            public string prodi
+            {
+                get { return _prodi; }
+                set { _prodi = value; }
+            }
+
+            [DataMember]
+            public string angkatan
+            {
+                get { return _angkatan; }
+                set { _angkatan = value; }
+            }
+
+        }
+
+        string baseUrl = "http://localhost:1907/";
+
+        void BuatMahasiswa()
+        {
+            Mahasiswa mahasiswa = new Mahasiswa();
+            Console.Write("Masukkan Nama: ");
+            mahasiswa.nama = Console.ReadLine();
+            Console.Write("Masukkan NIM: ");
+            mahasiswa.nim = Console.ReadLine();
+            Console.Write("Masukkan Prodi: ");
+            mahasiswa.prodi = Console.ReadLine();
+            Console.Write("Masukkan Angkatan: ");
+            mahasiswa.angkatan = Console.ReadLine();
+
+            var data = JsonConvert.SerializeObject(mahasiswa);
+            var postdata = new WebClient();
+            postdata.Headers.Add(HttpRequestHeader.ContentType, "application/json");
+            string response = postdata.UploadString(baseUrl + "Mahasiswa", data);
+            Console.WriteLine(response);
+        }
+
+        static void Main(string[] args)
+        {
+            ClassData classData = new ClassData();
+            classData.getData();
+            Program program = new Program();
+            program.BuatMahasiswa();
+            Console.ReadLine();
+        }
+    }
+}
